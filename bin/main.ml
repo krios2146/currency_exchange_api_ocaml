b7ee6ae1310ea332392f2a8787c9a2b625764b84
Vladimir Kidyaev <krios2146@gmail.com>
2024-09-02 04:03:18 +0700

    feat: update hello world example to respond with json & add logging + router

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = Dream.run (fun _ -> Dream.html "Hello, World!")
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+type message = { message : string } [@@deriving yojson]
+
+let hello_world _ =
+  { message = "Hello, world!" }
+  |> yojson_of_message |> Yojson.Safe.to_string |> Dream.json
+
+let () = Dream.run @@ Dream.logger @@ Dream.router [ Dream.get "/" hello_world ]
