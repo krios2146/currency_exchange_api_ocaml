51d89f68ca3ec06d0570bca9042e99fcf315011e
Vladimir Kidyaev <krios2146@gmail.com>
2024-09-04 02:42:42 +0700

    feat: add `add_currency` handler

--- lib/handlers/handlers.ml
+++ lib/handlers/handlers.ml
@@ -26,6 +26,14 @@ let respond_bad_request message =
   let message = build_message_response message in
   Dream.respond ~status:`Bad_Request ~headers:[ json_header ] message
 
+let respond_conflict message =
+  let message = build_message_response message in
+  Dream.respond ~status:`Conflict ~headers:[ json_header ] message
+
+let respond_server_error message =
+  let message = build_message_response message in
+  Dream.respond ~status:`Internal_Server_Error ~headers:[ json_header ] message
+
 let not_implemented _ =
   let message = build_message_response "This endpoint is not implemented yet" in
   Dream.respond ~status:`Not_Implemented ~headers:[ json_header ] message
@@ -59,5 +67,24 @@ let get_currency_by_code req =
           respond_bad_request
             (Printf.sprintf "Currency with code: '%s' is not found" code))
 
+let add_currency req =
+  let%lwt form = Dream.form ~csrf:false req in
+  match form with
+  | `Ok [ ("code", code); ("name", _); ("sign", _) ]
+    when not (is_possibly_valid_code code) ->
+      respond_bad_request "Parameter code is invalid; Use ISO-4217 format"
+  | `Ok [ ("code", code); ("name", name); ("sign", sign) ] -> (
+      let%lwt result =
+        Dream.sql req (Repository.save_currency code name sign)
+      in
+      match result with
+      | Ok currency ->
+          let saved_currency =
+            currency |> to_response_currency |> yojson_of_currency |> to_string
           in
-          Dream.respond ~status:`Not_Found ~headers:[ json_header ] message)
+          Dream.respond ~status:`Created ~headers:[ json_header ] saved_currency
+      | Error Constraint_violation ->
+          respond_conflict "This currency already exists"
+      | Error Unknown_error ->
+          respond_server_error "Server unable to process request")
+  | _ -> respond_bad_request "Unexpected / missing form values"
--- lib/handlers/handlers.mli
+++ lib/handlers/handlers.mli
@@ -1,3 +1,4 @@
 val not_implemented : Dream.request -> Dream.response Lwt.t
 val get_currencies : Dream.request -> Dream.response Lwt.t
 val get_currency_by_code : Dream.request -> Dream.response Lwt.t
+val add_currency : Dream.request -> Dream.response Lwt.t
