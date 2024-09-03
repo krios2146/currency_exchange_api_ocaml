c4d791b81e785503a656fb13d4391783bad41c6a
Vladimir Kidyaev <krios2146@gmail.com>
2024-09-04 05:20:22 +0700

    feat: add handler for `GET /exchangeRate/:codes` endpoint

--- bin/main.ml
+++ bin/main.ml
@@ -7,8 +7,8 @@ let () =
          Dream.get "/currency/:code" Handlers.get_currency_by_code;
          Dream.post "/currencies" Handlers.add_currency;
          Dream.get "/exchangeRates" Handlers.get_exchange_rates;
-         Dream.get "/exchnageRate/:codes" Handlers.not_implemented;
+         Dream.get "/exchangeRate/:codes" Handlers.get_exchange_rate_by_codes;
          Dream.post "/exchangeRates" Handlers.not_implemented;
-         Dream.patch "/exchnageRate/:codes" Handlers.not_implemented;
-         Dream.get "/exchnage" Handlers.not_implemented;
+         Dream.patch "/exchangeRate/:codes" Handlers.not_implemented;
+         Dream.get "/exchange" Handlers.not_implemented;
        ]
