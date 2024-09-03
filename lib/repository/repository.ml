cd29b3b5e72d05c5938389bf85f8d4f2995f7fb1
Vladimir Kidyaev <krios2146@gmail.com>
2024-09-04 04:02:29 +0700

    feat: add `find_all_exchange_rates` fun to the repository

--- lib/repository/repository.ml
+++ lib/repository/repository.ml
@@ -3,12 +3,51 @@ open Caqti_request.Infix
 module type DB = Caqti_lwt.CONNECTION
 
 type currency = { id : int; code : string; full_name : string; sign : string }
+
+type exchange_rate = {
+  id : int;
+  base_currency : currency;
+  target_currency : currency;
+  rate : float;
+}
+
 type error = Constraint_violation | Unknown_error
 
 let currency_t =
-  let encode c = Ok (c.id, c.code, c.full_name, c.sign) in
+  let encode (c : currency) = Ok (c.id, c.code, c.full_name, c.sign) in
   let decode (id, code, full_name, sign) = Ok { id; code; full_name; sign } in
   Caqti_type.(t4 int string string string) |> Caqti_type.custom ~encode ~decode
+
+let exchange_rate_t =
+  let encode (er : exchange_rate) =
+    Ok
+      ( er.id,
+        ( er.base_currency.id,
+          er.base_currency.code,
+          er.base_currency.full_name,
+          er.base_currency.sign ),
+        ( er.target_currency.id,
+          er.target_currency.code,
+          er.target_currency.full_name,
+          er.target_currency.sign ),
+        er.rate )
+  in
+  let decode
+      ( id,
+        (b_id, b_code, b_full_name, b_sign),
+        (t_id, t_code, t_full_name, t_sign),
+        rate ) =
+    let base_currency =
+      { id = b_id; code = b_code; full_name = b_full_name; sign = b_sign }
+    in
+    let target_currency =
+      { id = t_id; code = t_code; full_name = t_full_name; sign = t_sign }
+    in
+    Ok { id; base_currency; target_currency; rate }
+  in
+  Caqti_type.(
+    t4 int (t4 int string string string) (t4 int string string string) float)
+  |> Caqti_type.custom ~encode ~decode
 
 let find_all_currencies =
   let query =
@@ -47,3 +86,20 @@ let save_currency code name sign =
             Lwt_result.fail Constraint_violation
         | _ -> Lwt_result.fail Unknown_error)
     | Error _ -> Lwt_result.fail Unknown_error
+
+let find_all_exchange_rates =
+  let query =
+    (Caqti_type.unit ->* exchange_rate_t)
+      "SELECT e.id AS id, b.id as b_id, b.code as b_code, b.full_name as \
+       b_full_name, b.sign as b_sign, t.id as t_id, t.code as t_code, \
+       t.full_name as t_full_name, t.sign as t_sign, e.rate as rate FROM \
+       Exchange_rates e JOIN Currencies b ON e.base_currency_id = b.id JOIN \
+       Currencies t ON e.target_currency_id = t.id"
+  in
+  fun (module Db : DB) ->
+    let%lwt result = Db.collect_list query () in
+    match result with
+    | Ok currency -> Lwt_result.return currency
+    | Error e ->
+        Dream.log "Error: %s" (Caqti_error.show e);
+        Lwt_result.fail Unknown_error
--- lib/repository/repository.mli
+++ lib/repository/repository.mli
@@ -1,6 +1,14 @@
 module type DB = Caqti_lwt.CONNECTION
 
 type currency = { id : int; code : string; full_name : string; sign : string }
+
+type exchange_rate = {
+  id : int;
+  base_currency : currency;
+  target_currency : currency;
+  rate : float;
+}
+
 type error = Constraint_violation | Unknown_error
 
 val find_all_currencies : (module DB) -> (currency list, error) result Lwt.t
@@ -10,3 +18,6 @@ val find_currency_by_code :
 
 val save_currency :
   string -> string -> string -> (module DB) -> (currency, error) result Lwt.t
+
+val find_all_exchange_rates :
+  (module DB) -> (exchange_rate list, error) result Lwt.t
